6c34213cf357008270f5abc5aacbc7d192fd0e3b
vaidehijagtap <112371822+vaidehijagtap@users.noreply.github.com>
2024-04-10 01:05:01 -0700

    .

--- LAB3/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_pprior_quizz_QuizzApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.pprior.quizz.QuizzApp"
-)
-public final class _com_pprior_quizz_QuizzApp {
-}
